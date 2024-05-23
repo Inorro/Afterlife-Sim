6d58a36d41e44cb55683b40a7f4995fde852eac6
unknown <ronimujku@gmail.com>
2024-05-23 14:10:33 +0100

    1st commit

--- Scripts/Buttons.gd
+++ Scripts/Buttons.gd
@@ -1,9 +1,14 @@
 extends Node
 
 var button_functions : Dictionary
+@onready var game = get_parent()
 @onready var button1 = $Button
 @onready var button2 = $Button2
 @onready var label = $"../Label"
+@onready var paper = $"../Paper"
+@onready var paper_2 = $"../Paper2"
+@onready var paper_3 = $"../Paper3"
+
 
 func _ready():
 	
@@ -21,7 +26,18 @@ func some_button_pressed(button):
 func accept():
 	label.visible = true
 	label.text = "{Name} accepted into heaven"
+	await get_tree().create_timer(1.0).timeout
+	paper.hide()
+	paper_2.hide()
+	paper_3.hide()
+	game.reset()
+	
 	
 func deny():
 	label.visible = true
 	label.text = "{Name} sent to hell"
+	await get_tree().create_timer(1.0).timeout
+	paper.queue_free()
+	paper_2.queue_free()
+	paper_3.queue_free()
+	game.reset()
--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -3,19 +3,35 @@ extends Node2D
 var paper_stack = []
 
 var paper_scene = preload("res://Scenes/Paper.tscn")
+var scene = preload("res://Scenes/Paper.tscn")
+@onready var button = $Buttons/Button
+@onready var button2 = $Buttons/Button2
+@onready var paper1 = $Paper
+@onready var paper2 = $Paper2
+@onready var paper3 = $Paper3
+@onready var paper1l = $Paper/Label
+@onready var paper2l = $Paper2/Label
+@onready var paper3sprite = $Paper3/Sprite2D
+@onready var paper3l = $Paper3/Label
+@onready var paper3main = $Paper3/Label2
+@onready var paper1main = $Paper/Label2
+@onready var paper2main = $Paper2/Label2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var button = $Buttons/Button
-	var button2 = $Buttons/Button2
-	var paper1 = $Paper/Label
-	var paper2 = $Paper2/Label
-	var paper1main = $Paper/Label2
-	var paper2main = $Paper2/Label2
+	var file = FileAccess.open("res://Data/localization.json", FileAccess.READ)
+	var data = JSON.parse_string(file.get_as_text())
+	var file_data = data
+	print(file_data)
+	
 	button.set_text("Accept")
 	button2.set_text("Deny")
-	paper1.text = "Sins"
-	paper2.text = "Virtues"
+	paper1l.text = "Sins"
+	paper2l.text = "Virtues"
+	paper3sprite.texture = load("res://Assets/pixil-frame-0.png")
+	paper3l.text= ""
+	paper3main.text= ""
+	paper3sprite.scale = Vector2(3, 3)
 	paper1main.text = "Killed Diana
 	Drugs
 	Doesnt flush"
@@ -23,9 +39,7 @@ func _ready():
 	
 	for i in range(0, 2):
 		var paper = paper_scene.instantiate()
-		add_paper(paper)
-	
-	
+		add_paper(paper)	
 	
 
 func add_paper(paper):
@@ -34,9 +48,14 @@ func add_paper(paper):
 	var count = 0
 	for p in paper_stack:
 		p.z_index = count
-		
 		count += 1
 
 func push_paper_to_top(paper):
 	paper_stack.erase(paper)
 	add_paper(paper)
+	
+func reset():
+	await get_tree().create_timer(1.0).timeout
+	paper1.show()
+	paper2.show()
+	paper3.show()
